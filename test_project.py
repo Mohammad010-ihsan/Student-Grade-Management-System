345f257695ec85c929cc40d323fa896bf01c3e3e
Mohammad010-ihsan <mo.ha0100989@gmail.com>
2026-05-04 15:35:18 +0300

    Uncomment test functions in test_project.py

--- test_project.py
+++ test_project.py
@@ -1,24 +1,24 @@
-#from project import (
-#    calculate_gpa_100,
-#    convert_to_gpa_4,
-#    classify_gpa_4,
-#    save_students_to_csv,
-#)
+from project import (
+    calculate_gpa_100,
+    convert_to_gpa_4,
+    classify_gpa_4,
+    save_students_to_csv,
+)
 
 
-#def test_calculate_gpa_100():
-#    assert calculate_gpa_100([90, 80, 70]) == 80
-#    assert calculate_gpa_100([]) == 0
+def test_calculate_gpa_100():
+    assert calculate_gpa_100([90, 80, 70]) == 80
+    assert calculate_gpa_100([]) == 0
 
 
-#def test_convert_to_gpa_4():
-#    assert convert_to_gpa_4(100) == 4.0
-#    assert convert_to_gpa_4(50) == 2.0
+def test_convert_to_gpa_4():
+    assert convert_to_gpa_4(100) == 4.0
+    assert convert_to_gpa_4(50) == 2.0
 
 
-#def test_classify_gpa_4():
-#    assert classify_gpa_4(3.8) == "Excellent"
-#    assert classify_gpa_4(3.2) == "Very Good"
-#    assert classify_gpa_4(2.5) == "Good"
-#    assert classify_gpa_4(1.5) == "Pass"
-#    assert classify_gpa_4(0.5) == "Fail"
+def test_classify_gpa_4():
+    assert classify_gpa_4(3.8) == "Excellent"
+    assert classify_gpa_4(3.2) == "Very Good"
+    assert classify_gpa_4(2.5) == "Good"
+    assert classify_gpa_4(1.5) == "Pass"
+    assert classify_gpa_4(0.5) == "Fail"
